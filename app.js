8a53cf8ea41b26b3da66ef8b8e477958a171ec58
Sea Grass <dev.seagrass@gmail.com>
2014-08-08 20:44:08 +0000

    Add some comments to app.js

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
+//LOGIC
+//Create the players. This must be an Array[2]
 var players = [0,1];
+//The game starts on player 0's turn. Possible values for turn are [0,1]
 var turn = 0;
 var grid = [];
-var container = document.createElement("div");
-container.classList.add("tic-tac-toe-board");
+
 var gameActionListener = function() {
 //This gets called every time a user makes an action (click or touch)
 	return function(e) {
@@ -23,8 +25,11 @@ var gameActionListener = function() {
 		ui.update();
 	};
 }();
-container.addEventListener("click", gameActionListener);
-container.addEventListener("touch", gameActionListener);
+
+//UI
+//Create the tic-tac-toe board
+var container = document.createElement("div");
+container.classList.add("tic-tac-toe-board");
 for (var i = 0; i < 9; i++) {
 	var el = document.createElement("div");
 	el.classList.add("grid-square");
@@ -33,18 +38,29 @@ for (var i = 0; i < 9; i++) {
 	el.innerHTML = i;
 	container.appendChild(grid[i]);
 }
-document.body.appendChild(container);
 
-var ui = document.createElement("div");
+//Execute a turn whenever a user clicks or touches the tic-tac-toe board
+container.addEventListener("click", gameActionListener);
+container.addEventListener("touch", gameActionListener);
+
+//Add the tic-tac-toe board to the page
+document.body.appendChild(container);
+//Add the tic-tac-toe game status UI to the page
 document.body.appendChild(ui);
+
+//Create the game status UI
+var ui = document.createElement("div");
 ui.classList.add("ui");
 ui.innerHTML = "<div id='players'></div>";
 ui.innerHTML += "<div id='turns'></div>";
+
+//Create a function to show a message when the game is completed
 ui.gameover = function(winner) {
 	this.innerHTML = "Game over!";
 	this.innerHTML += winner+" wins!! haha";
 	console.log(grid);
 }
+//Create a function to update the game status after each turn
 ui.update = function() {
 	if (turn == players[0]) {
 		this.querySelector("#players").innerHTML = "<b>"+players[0]+"</b>" + " : " + players[1];
@@ -62,5 +78,7 @@ ui.update = function() {
 		ui.gameover("X");
 	}
 };
+
+//Initialize the game status UI values
 ui.update();
 
